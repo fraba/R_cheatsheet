7afb20631a6041b829fb656ff8a144b302a125a1
Francesco Bailo <me@francescobailo.net>
2015-12-05 10:18:02 +0100

    Added basic database functions

--- database.R
+++ database.R
@@ -2,6 +2,24 @@
 
 ## Database connection
 
+sqliteGetTable <- function(database, table) {
+  require(DBI)
+  require(RSQLite)
+  con <- dbConnect(RSQLite::SQLite(), dbname = database)
+  query <- dbSendQuery(con, paste("SELECT * FROM ", table, ";", sep="")) 
+  result <- fetch(query, n = -1)
+  dbClearResult(query)
+  dbDisconnect(con)
+  return(result)
+}
+
+sqliteListTables <- function(database, table) {
+  require(DBI)
+  require(RSQLite)
+  con <- dbConnect(RSQLite::SQLite(), dbname = database)
+  return(dbListTables(conn))
+}
+
 ## Database search
 
 
