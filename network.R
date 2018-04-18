3fb33d14a687056be997d9899c8a16f1eecbfe0e
Francesco Bailo <me@francescobailo.net>
2018-04-18 14:43:27 +1000

    Added igraph::

--- network.R
+++ network.R
@@ -11,7 +11,7 @@ addDegreeToVertices <- function (g) {
     V(g)$indegree <- igraph::degree(g, V(g), mode = 'in') 
     V(g)$outdegree <- igraph::degree(g, V(g), mode = 'out') 
   } else {
-    try(if('degree' %in% list.vertex.attributes(g)) stop("Vertex attribure degree already exists"))
+    try(if('degree' %in% igraph::list.vertex.attributes(g)) stop("Vertex attribure degree already exists"))
     V(g)$degree <- igraph::degree(g, V(g)) 
   }
   return(g)
@@ -19,12 +19,12 @@ addDegreeToVertices <- function (g) {
 
 vertexAttributesAsDataFrame <- function(g) {
   try(if(!(is.igraph(g))) stop("Not an igraph graph"))
-  vertex_attributes <- list.vertex.attributes(g)
+  vertex_attributes <- igraph::list.vertex.attributes(g)
   df <- data.frame(matrix(NA, nrow = vcount(g), ncol = 0))
   for (a in vertex_attributes) {
-    df <- cbind(df, data.frame(get.vertex.attribute(g, a)))
+    df <- cbind(df, data.frame(igraph::get.vertex.attribute(g, a)))
   }
-  names(df) <- list.vertex.attributes(g)
+  names(df) <- igraph::list.vertex.attributes(g)
   return(df)
 }
 
